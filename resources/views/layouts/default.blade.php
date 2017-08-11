b5243b23f71ebfdf3e60338f8850b6b16754191a
Alex Adusei <alex.a.adusei@gmail.com>
2017-08-11 23:49:49 +0000

    enabled scripts on dashboard

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -50,69 +50,69 @@
 </body>
 
 <!--   Core JS Files. Extra: TouchPunch for touch library inside jquery-ui.min.js   -->
-<script src="assets/js/jquery-3.1.1.min.js" type="text/javascript"></script>
-<script src="/assets/js/jquery-ui.min.js" type="text/javascript"></script>
-<script src="/assets/js/perfect-scrollbar.min.js" type="text/javascript"></script>
-<script src="/assets/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="{{asset('assets/js/jquery-3.1.1.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/js/jquery-ui.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/js/perfect-scrollbar.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/js/bootstrap.min.js')}}" type="text/javascript"></script>
 
 <!-- <script src="/assets/js/app.js"></script> -->
 
 <!--  Forms Validations Plugin -->
-<script src="/assets/js/jquery.validate.min.js"></script>
+<script src="{{asset('assets/js/jquery.validate.min.js')}}"></script>
 
 <!-- Promise Library for SweetAlert2 working on IE -->
-<script src="/assets/js/es6-promise-auto.min.js"></script>
+<script src="{{asset('assets/js/es6-promise-auto.min.js')}}"></script>
 
 <!--  Plugin for Date Time Picker and Full Calendar Plugin-->
-<script src="/assets/js/moment.min.js"></script>
+<script src="{{asset('assets/js/moment.min.js')}}"></script>
 
 <!--  Date Time Picker Plugin is included in this js file -->
-<script src="/assets/js/bootstrap-datetimepicker.js"></script>
+<script src="{{asset('assets/js/bootstrap-datetimepicker.js')}}"></script>
 
 <!--  Select Picker Plugin -->
-<script src="/assets/js/bootstrap-selectpicker.js"></script>
+<script src="{{asset('assets/js/bootstrap-selectpicker.js')}}"></script>
 
 <!--  Switch and Tags Input Plugins -->
-<script src="/assets/js/bootstrap-switch-tags.js"></script>
+<script src="{{asset('assets/js/bootstrap-switch-tags.js')}}"></script>
 
 <!-- Circle Percentage-chart -->
-<script src="/assets/js/jquery.easypiechart.min.js"></script>
+<script src="{{asset('assets/js/jquery.easypiechart.min.js')}}"></script>
 
 <!--  Charts Plugin -->
-<script src="/assets/js/chartist.min.js"></script>
+<script src="{{asset('assets/js/chartist.min.js')}}"></script>
 
 <!--  Notifications Plugin    -->
-<script src="/assets/js/bootstrap-notify.js"></script>
+<script src="{{asset('assets/js/bootstrap-notify.js')}}"></script>
 
 <!-- Sweet Alert 2 plugin -->
-<script src="/assets/js/sweetalert2.js"></script>
+<script src="{{asset('assets/js/sweetalert2.js')}}"></script>
 
 <!-- Vector Map plugin -->
-<script src="/assets/js/jquery-jvectormap.js"></script>
+<script src="{{asset('assets/js/jquery-jvectormap.js')}}"></script>
 
 <!--  Google Maps Plugin    -->
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAX1B71USyaSc9Jn8pM-0py8Mi7mySywLQ"></script>
 
 <!-- Wizard Plugin    -->
-<script src="/assets/js/jquery.bootstrap.wizard.min.js"></script>
+<script src="{{asset('')}}assets/js/jquery.bootstrap.wizard.min.js"></script>
 
 <!--  Bootstrap Table Plugin    -->
-<script src="/assets/js/bootstrap-table.js"></script>
+<script src="{{asset('assets/js/bootstrap-table.js')}}"></script>
 
 <!--  Plugin for DataTables.net  -->
-<script src="/assets/js/jquery.datatables.js"></script>
+<script src="{{asset('assets/js/jquery.datatables.js')}}"></script>
 
 <!--  Full Calendar Plugin    -->
-<script src="/assets/js/fullcalendar.min.js"></script>
+<script src="{{asset('assets/js/fullcalendar.min.js')}}"></script>
 
 <!-- Paper Dashboard PRO Core javascript and methods for Demo purpose -->
-<script src="/assets/js/paper-dashboard.js?v=1.2.1"></script>
+<script src="{{asset('assets/js/paper-dashboard.js?v=1.2.1')}}"></script>
 
 	<!--   Sharrre Library    -->
-	<script src="/assets/js/jquery.sharrre.js"></script>
+	<script src="{{asset('assets/js/jquery.sharrre.js')}}"></script>
 
 	<!-- Paper Dashboard PRO DEMO methods, don't include it in your project! -->
-<script src="/assets/js/demo.js"></script>
+<script src="{{asset('assets/js/demo.js')}}"></script>
 
 
 
