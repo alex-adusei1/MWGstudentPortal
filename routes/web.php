775df3a1f72de67473c47f4c985f48b709675e9b
Alex Adusei <alex.a.adusei@gmail.com>
2017-08-14 13:00:33 +0000

    enabled dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/dashboard', 'HomeController@index')->name('dashboard');
+Route::get('/home', 'HomeController@index')->name('dashboard');
 
 Route::get('/user', 'PagesController@student');
 
