2d5242535ba5d156c0ccb7d926d2790719e34091
Wisdom2 <agbenuwisdoms@gmail.com>
2017-08-15 16:05:00 +0100

    editted the registration
    removed the registration form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,9 +70,9 @@
                 <div class="top-right links">
                     @if (Auth::check())
                         <a href="{{ url('/home') }}">Home</a>
-                    @else
+                   {{-- @else
                         <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
+                         <a href="{{ url('/register') }}">Register</a> --}}
                     @endif
                 </div>
             @endif
