5e137b977f1b47064a6116d5ad176e6e2a98c801
Wisdom2 <agbenuwisdoms@gmail.com>
2017-09-04 21:49:00 +0100

    manage student update
    updated student view by fetching data from table

--- resources/views/pages/academic/class.blade.php
+++ resources/views/pages/academic/class.blade.php
@@ -1,7 +1,49 @@
+
+<!--Class-->
 @extends('layouts.default')
 
 @section('content')
 
-classes
+<div class="content">
+  <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="content">
+           <div class="container-fluid">
+               <div class="row">
+                   <div class="col-md-12">
+                       <div class="card">
+                           <div class="card-content">
+                               <div class="toolbar">
+                                   <!--Here you can write extra buttons/actions for the toolbar-->
+                               </div>
+                               <table id="bootstrap-table" class="table">
+                                   <thead>
+                                       <th data-field="state" data-checkbox="true"></th>
+                                       <th data-field="id" class="text-center">ID</th>
+                                     <th data-field="name" data-sortable="true">Class</th>
+                                     <th data-field="salary" data-sortable="true">Capacity</th>
+                                     <th data-field="country" data-sortable="true">Added on</th>
+                                   </thead>
+                                   <tbody>
+                                            @foreach($classes as $class)
+                                                <tr>
+                                                   <td>{{class->name}}</td>
+                                                   <td>{{class->capacity}}</td>
+                                                   <td>{{class->created_at}}</td>
+                                                </tr>
+                                            @endforeach
+                                  </tbody>
+                               </table>
+                           </div>
+                       </div><!--  end card  -->
+                   </div> <!-- end col-md-12 -->
+               </div> <!-- end row -->
+           </div>
+       </div>
+      </div>
+  </div>
+</div>
+</div>
 
 @endsection
